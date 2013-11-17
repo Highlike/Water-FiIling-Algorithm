64b6a8f754ac2041eaac230ea7ff23f737063128
Highlike <Kevin1995G@gmail.com>
2013-11-17 17:37:53 +0100

    fix indention

--- solution.php
+++ solution.php
@@ -1,51 +1,51 @@
 #!/usr/bin/env php
 <?php
 function max_from_left($values) {
-	$max = 0;
-	$max_values = array();
-	
-	foreach ($values as $i) {
-		if ($i > $max) {
-			$max = $i;
-		}
-		$max_values[] = $max;
-	}
-	
-	return $max_values;
+    $max = 0;
+    $max_values = array();
+    
+    foreach ($values as $i) {
+        if ($i > $max) {
+            $max = $i;
+        }
+        $max_values[] = $max;
+    }
+    
+    return $max_values;
 }
 
 function max_from_right($values) {
-	# pass max_from_left reversed values and reverse
-	return array_reverse(max_from_left(array_reverse($values)));
+    # pass max_from_left reversed values and reverse
+    return array_reverse(max_from_left(array_reverse($values)));
 }
 
 function water_volume($land) {
-	$l_max = max_from_left($land);
-	$r_max = max_from_right($land);
-	
-	assert(count($l_max) == count($r_max));
-	
-	# contour = water + land
-	$contour = array();
-	for ($i = 0; $i < count($l_max); $i++) {
-		$x = $l_max[$i];
-		$y = $r_max[$i];
-		$contour[] = min($x, $y);
-	}
+    $l_max = max_from_left($land);
+    $r_max = max_from_right($land);
+    
+    assert(count($l_max) == count($r_max));
+    
+    # contour = water + land
+    $contour = array();
+    for ($i = 0; $i < count($l_max); $i++) {
+        $x = $l_max[$i];
+        $y = $r_max[$i];
+        $contour[] = min($x, $y);
+    }
 
-	assert(count($land) == count($contour));
-	
-	# water = contour - land
-	$water = array();
-	for ($i = 0; $i < count($land); $i++) {
-		$x = $contour[$i];
-		$y = $land[$i];
-		$water[] = $x - $y;
-	}
-	
-	$volume = array_sum($water);
-	
-	return $volume;
+    assert(count($land) == count($contour));
+    
+    # water = contour - land
+    $water = array();
+    for ($i = 0; $i < count($land); $i++) {
+        $x = $contour[$i];
+        $y = $land[$i];
+        $water[] = $x - $y;
+    }
+    
+    $volume = array_sum($water);
+    
+    return $volume;
 }
 
 $land = array(2, 3, 5, 1, 2, 3, 4, 7, 7, 6);
--- solution.py
+++ solution.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python
 
 def max_from_left(values):
-	max = 0
-	max_values = list()
+    max = 0
+    max_values = list()
 
-	for i in values:
-		if i > max:
-			max = i
+    for i in values:
+        if i > max:
+            max = i
 
-		max_values.append(max)
+        max_values.append(max)
 
-	return max_values
+    return max_values
 
 def max_from_right(values):
-	# pass max_from_left reversed values and reverse
-	return reversed(max_from_left(reversed(values)))
+    # pass max_from_left reversed values and reverse
+    return reversed(max_from_left(reversed(values)))
 
 def water_volume(land):
-	l_max = max_from_left(land)
-	r_max = max_from_right(land)
+    l_max = max_from_left(land)
+    r_max = max_from_right(land)
 
-	# contour = water + land
-	contour = [min(x, y) for x, y in zip(l_max, r_max)]
+    # contour = water + land
+    contour = [min(x, y) for x, y in zip(l_max, r_max)]
 
-	# water = contour - land
-	water = [x - y for x, y in zip(contour, land)]
+    # water = contour - land
+    water = [x - y for x, y in zip(contour, land)]
 
-	volume = sum(water)
+    volume = sum(water)
 
-	return volume
+    return volume
 
 if __name__ == '__main__':
-	land = [2, 3, 5, 1, 2, 3, 4, 7, 7, 6]
+    land = [2, 3, 5, 1, 2, 3, 4, 7, 7, 6]
 
-	print(water_volume(land))
+    print(water_volume(land))
